cdf5725a213efeb77488e528959b5e5cf8855c16
Andrii7777 <andy.magaletskyi@gmail.com>
2024-07-20 13:27:34 +0200

    Fix checkstyle-suppersesion and fix code style issues

--- src/main/java/com/to/ordermanagementservice/repository/impl/InMemoryOrderRepository.java
+++ src/main/java/com/to/ordermanagementservice/repository/impl/InMemoryOrderRepository.java
@@ -7,15 +7,22 @@ import org.springframework.stereotype.Repository;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ID_1;
+import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ID_2;
+import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ID_3;
+import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.USER_ID_1;
+import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.USER_ID_2;
+import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.USER_ID_3;
+
 @Repository
 public class InMemoryOrderRepository implements OrderRepository {
 
     private final List<Order> orders;
     public InMemoryOrderRepository() {
         orders = List.of(
-                createOrder(1, 1),
-                createOrder(2, 2),
-                createOrder(3, 3)
+                createOrder(ORDER_ID_1, USER_ID_1),
+                createOrder(ORDER_ID_2, USER_ID_2),
+                createOrder(ORDER_ID_3, USER_ID_3)
         );
     }
 
---
+++ src/main/java/com/to/ordermanagementservice/utils/InMemoryRepositoryUtils.java
@@ -0,0 +1,21 @@
+package com.to.ordermanagementservice.utils;
+
+public final class InMemoryRepositoryUtils {
+    public static final int ORDER_ID_1 = 1;
+    public static final int ORDER_ID_2 = 2;
+    public static final int ORDER_ID_3 = 3;
+    public static final int USER_ID_1 = 1;
+    public static final int USER_ID_2 = 2;
+    public static final int USER_ID_3 = 3;
+
+    public static final int ORDER_ITEM_ID_11 = 11;
+    public static final int ORDER_ITEM_ID_21 = 21;
+    public static final int ORDER_ITEM_ID_22 = 22;
+
+    public static final int ORDER_ITEM_QUANTITY_WITH_ID_11 = 10;
+    public static final int ORDER_ITEM_QUANTITY_WITH_ID_21 = 110;
+    public static final int ORDER_ITEM_QUANTITY_WITH_ID_22 = 4;
+
+    private InMemoryRepositoryUtils() {
+    }
+}
