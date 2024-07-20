82169a9e2ea75716069e76c28862b139c434a569
Andrii7777 <andy.magaletskyi@gmail.com>
2024-07-20 15:05:50 +0200

    replace files and fix bug

--- src/main/java/com/to/ordermanagementservice/repository/impl/InMemoryOrderItemRepository.java
+++ src/main/java/com/to/ordermanagementservice/repository/impl/InMemoryOrderItemRepository.java
@@ -9,15 +9,6 @@ import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ID_1;
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ID_2;
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ITEM_ID_11;
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ITEM_ID_21;
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ITEM_ID_22;
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ITEM_QUANTITY_WITH_ID_11;
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ITEM_QUANTITY_WITH_ID_21;
-import static com.to.ordermanagementservice.utils.InMemoryRepositoryUtils.ORDER_ITEM_QUANTITY_WITH_ID_22;
-
 @Repository
 public class InMemoryOrderItemRepository implements OrderItemRepository {
 
@@ -25,28 +16,28 @@ public class InMemoryOrderItemRepository implements OrderItemRepository {
 
     public InMemoryOrderItemRepository() {
         OrderItem orderItem11 = new OrderItem();
-        orderItem11.setId(ORDER_ITEM_ID_11);
-        orderItem11.setOrderId(ORDER_ID_1);
+        orderItem11.setId(11);
+        orderItem11.setOrderId(1);
         orderItem11.setProductId(1);
-        orderItem11.setQuantity(ORDER_ITEM_QUANTITY_WITH_ID_11);
+        orderItem11.setQuantity(10);
         orderItem11.setCreatedAt(OffsetDateTime.now());
         orderItem11.setUpdatedAt(OffsetDateTime.now());
         orderItem11.setPrice(new BigDecimal("12.50"));
 
         OrderItem orderItem21 = new OrderItem();
-        orderItem21.setId(ORDER_ITEM_ID_21);
-        orderItem21.setOrderId(ORDER_ID_2);
+        orderItem21.setId(21);
+        orderItem21.setOrderId(2);
         orderItem21.setProductId(2);
-        orderItem21.setQuantity(ORDER_ITEM_QUANTITY_WITH_ID_21);
+        orderItem21.setQuantity(110);
         orderItem21.setCreatedAt(OffsetDateTime.now());
         orderItem21.setUpdatedAt(OffsetDateTime.now());
         orderItem21.setPrice(new BigDecimal("42.50"));
 
         OrderItem orderItem22 = new OrderItem();
-        orderItem22.setId(ORDER_ITEM_ID_22);
-        orderItem22.setOrderId(ORDER_ID_2);
+        orderItem22.setId(22);
+        orderItem22.setOrderId(2);
         orderItem22.setProductId(2);
-        orderItem22.setQuantity(ORDER_ITEM_QUANTITY_WITH_ID_22);
+        orderItem22.setQuantity(4);
         orderItem22.setCreatedAt(OffsetDateTime.now());
         orderItem22.setUpdatedAt(OffsetDateTime.now());
         orderItem22.setPrice(new BigDecimal("110"));
