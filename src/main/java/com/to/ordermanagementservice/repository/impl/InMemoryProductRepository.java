b6beb9174a99f3b3c2601c39aecb898ed7468571
Andrii7777 <andy.magaletskyi@gmail.com>
2024-07-25 10:56:04 +0200

    change product list and fix bug

--- src/main/java/com/to/ordermanagementservice/repository/impl/InMemoryProductRepository.java
+++ src/main/java/com/to/ordermanagementservice/repository/impl/InMemoryProductRepository.java
@@ -1,6 +1,8 @@
 package com.to.ordermanagementservice.repository.impl;
 
+import com.to.ordermanagementservice.entity.Clothing;
 import com.to.ordermanagementservice.entity.Electronic;
+import com.to.ordermanagementservice.entity.Grocery;
 import com.to.ordermanagementservice.entity.Product;
 import com.to.ordermanagementservice.repository.ProductRepository;
 import org.springframework.stereotype.Repository;
@@ -23,18 +25,17 @@ public class InMemoryProductRepository implements ProductRepository {
                 "SAMSUNG",
                 LocalDate.now());
         products.add(electronic1);
-        Product electronic2 = new Electronic(2,
-                "PS",
-                new BigDecimal(25000),
-                "SONY",
+        Grocery grocery1 = new Grocery(2,
+                "bread",
+                new BigDecimal(23),
                 LocalDate.now());
-        products.add(electronic2);
-        Product electronic3 = new Electronic(3,
-                "Phone",
-                new BigDecimal(300),
-                "Nokia",
-                LocalDate.now());
-        products.add(electronic3);
+        products.add(grocery1);
+        Clothing clothing1 = new Clothing(3,
+                "ClubTS",
+                new BigDecimal(20),
+                50,
+                "Blue");
+        products.add(clothing1);
     }
 
     @Override
